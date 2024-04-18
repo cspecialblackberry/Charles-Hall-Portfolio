8e884f37eb4748265ca2adbd7e681157304b5c79
Hall Family <charliesamhall@icloud.com>
2024-04-18 09:28:14 -0400

    further progress on portfolio page

--- react-portfolio/src/assets/sites.js
+++ react-portfolio/src/assets/sites.js
@@ -6,4 +6,18 @@ export const sites = [
         description: 'MediMinder is an app that allows users to enter medications they take and what times they take them. At these times, the site will send notifications.',
         img: 'src/assets/mediminder.png',
     },
+    {
+        name: 'MediMinder',
+        link: 'https://mediminder-e3e3cf63329a.herokuapp.com/',
+        repo: 'https://github.com/cspecialblackberry/mediminder-app',
+        description: 'MediMinder is an app that allows users to enter medications they take and what times they take them. At these times, the site will send notifications.',
+        img: 'src/assets/mediminder.png',
+    },
+    {
+        name: 'MediMinder',
+        link: 'https://mediminder-e3e3cf63329a.herokuapp.com/',
+        repo: 'https://github.com/cspecialblackberry/mediminder-app',
+        description: 'MediMinder is an app that allows users to enter medications they take and what times they take them. At these times, the site will send notifications.',
+        img: 'src/assets/mediminder.png',
+    },
 ]
--- react-portfolio/src/pages/Portfolio.jsx
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -1,19 +1,25 @@
 import { Link, Card, CardHeader, CardBody, Button } from '@chakra-ui/react'
 
-export default function Portfolio(props) {
-    const {sites} = props
+export default function Portfolio({sites}) {
     console.log(sites)
     return(
         <>
         portfolio
-        {sites.map((site) => {
+        <div className='sites-container'>
+        {sites.map((site) => 
             <>
-            <Card>
-                <CardHeader>{site.name}</CardHeader>
-                <img src={site.image}></img>
-            </Card>
+            <div className='site-div'>
+                <h2>{site.name}</h2>
+                <img src={site.img}></img>
+                <p>{site.description}</p>
+                <div>
+                    <a href={site.link} target='_blank'>Deployed Site</a>
+                    <a href={site.repo} target='_blank'>GitHub Repo</a>
+                </div>
+            </div>
             </>
-        })}
+        )}
+        </div>
         </>
     )
 }
