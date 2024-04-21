8e7ada73f3027bf9d6088c11371938d43b89f684
Hall Family <charliesamhall@icloud.com>
2024-04-21 13:04:11 -0400

    rearranged file structure

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,9 +5,9 @@ export default function Portfolio({ sites }) {
         <>
             <h1 style={{ fontSize: '40px', textAlign: 'center', fontWeight: '600', paddingTop: '25px' }}>Check Out My Work!</h1>
             <div className='sites-container'>
+                {/* create project component for each site */}
                 {sites.map((site) => <Project site={site} />)}
             </div>
         </>
     )
 }
-//todo make site-div own component
