f34db0465668e1f8c1a719b761343b74127d8fd7
Hall Family <charliesamhall@icloud.com>
2024-04-21 14:04:54 -0400

    moved images to public folder

--- src/assets/sites.js
+++ src/assets/sites.js
@@ -5,7 +5,7 @@ export const sites = [
         link: 'https://mediminder-e3e3cf63329a.herokuapp.com/',
         repo: 'https://github.com/cspecialblackberry/mediminder-app',
         description: 'MediMinder is an app that allows users to enter medications they take and what times they take them. At these times, the site will send notifications.',
-        img: 'src/assets/mediminder.png',
+        img: '../../public/mediminder.png',
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ export const sites = [
         link: 'https://quick-chef-app.onrender.com/',
         repo: 'https://github.com/caseynewman/quick-chef-app',
         description: 'QuickChef allows users to find simple recipes based off of a specified ingredient, how much time they have, and any dietary restrictions they have. Users can also save favorite recipes and add them to a weekly meal planner.',
-        img: 'src/assets/QuickChef.png',
+        img: '../../public/QuickChef.png',
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ export const sites = [
         link: 'https://cspecialblackberry.github.io/5-Day-Weather-Forecast/',
         repo: 'https://github.com/cspecialblackberry/5-Day-Weather-Forecast',
         description: '5 Day Weather Forecast allows users to see a five day forecast of any city they search for. Past searches will appear in the search history section.',
-        img: 'src/assets/weatherForecast.png',
+        img: '../../public/weatherForecast.png',
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ export const sites = [
         link: 'https://cspecialblackberry.github.io/JavaScript-Quiz-Game/',
         repo: 'https://github.com/cspecialblackberry/JavaScript-Quiz-Game',
         description: 'JavaScript Brain Trip is a simple quiz site that allows JS developers to test their knowledge of their language! Users must get all 10 questions correct within the time limit to win.',
-        img: 'src/assets/javascriptBrainTrip.png',
+        img: '../../public/javascriptBrainTrip.png',
     },
    
 ]
